ab5718b9b5019e4652bdb8d6066f26c1b84132f5
Stefan Sundin <git@stefansundin.com>
2016-11-01 21:28:39 -0700

    Remove console.log statements.

--- firefox/background.js
+++ firefox/background.js
@@ -1,18 +1,10 @@
-chrome.commands.getAll(function(commands) {
-  commands.forEach(function(command) {
-    console.log(command);
-  });
-});
-
 chrome.commands.onCommand.addListener(function(command) {
   if (command == "duplicate-tab") {
-    console.log("Duplicating tab..");
     chrome.tabs.query({
       currentWindow: true,
       active: true,
     }, function(tabs) {
       var tab = tabs[0]
-      console.log(tab);
       chrome.tabs.duplicate(tab.id);
     });
   }
