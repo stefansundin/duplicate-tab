7b73d3fe7719ce06b66a0a1dc3d62b59db214608
Stefan Sundin <git@stefansundin.com>
2016-11-01 21:48:19 -0700

    Add Chrome version.

---
+++ chrome/background.js
@@ -0,0 +1,11 @@
+chrome.commands.onCommand.addListener(function(command) {
+  if (command == "duplicate-tab") {
+    chrome.tabs.query({
+      currentWindow: true,
+      active: true,
+    }, function(tabs) {
+      var tab = tabs[0]
+      chrome.tabs.duplicate(tab.id);
+    });
+  }
+});
