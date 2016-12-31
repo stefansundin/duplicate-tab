e3c4ab157bdad4c9ded7c36cdb3fd1a5c4c36e41
Stefan Sundin <git@stefansundin.com>
2016-12-30 21:33:46 -0800

    I knew about this but forgot.

--- chrome/background.js
+++ chrome/background.js
@@ -4,7 +4,7 @@ chrome.commands.onCommand.addListener(function(command) {
       currentWindow: true,
       active: true,
     }, function(tabs) {
-      var tab = tabs[0]
+      var tab = tabs[0];
       chrome.tabs.duplicate(tab.id);
     });
   }
