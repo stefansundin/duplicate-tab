8b5c74180cae25eae4b4d4a04aaa2c4082b53992
Stefan Sundin <git@stefansundin.com>
2016-12-31 11:42:22 -0800

    Add mac scripts.

---
+++ make-xpi-mac.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -ex
+V=$(cat firefox/manifest.json | grep '"version"' | grep -o "\d*\.\d*\.\d*")
+rm -f "duplicate-tab-$V.xpi"
+cd firefox
+zip -r "../duplicate-tab-$V.xpi" . -x '*.DS_Store' -x '*Thumbs.db'
---
+++ make-zip-mac.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -ex
+V=$(cat chrome/manifest.json | grep '"version"' | grep -o "\d*\.\d*\.\d*")
+rm -f "duplicate-tab-$V.zip"
+cd chrome
+zip -r "../duplicate-tab-$V.zip" . -x '*.DS_Store' -x '*Thumbs.db'
