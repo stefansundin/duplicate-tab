3048df1f80a58da35d9aee626d1eb071bf41922c
Stefan Sundin <git@stefansundin.com>
2018-06-08 16:29:51 -0700

    v1.4.0

--- make-xpi.sh
+++ make-xpi.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -ex
 V=$(cat firefox/manifest.json | jq -Mr .version)
 rm -f "duplicate-tab-$V.xpi"
+
+grep -r console.log firefox && exit
+
 cd firefox
 zip -r "../duplicate-tab-$V.xpi" . -x '*.DS_Store' -x '*Thumbs.db'
+
+diff ../chrome/manifest.json manifest.json
