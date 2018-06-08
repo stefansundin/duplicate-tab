4332399b791758207881df92c6efee70cb176ddc
Stefan Sundin <git@stefansundin.com>
2018-06-08 11:34:29 -0700

    Switch back to using the duplicate function in Firefox when not opening tabs in the background, to preserve the tab history when possible.

--- firefox/background.js
+++ firefox/background.js
@@ -4,19 +4,32 @@ var default_options = {
 
 chrome.commands.onCommand.addListener(function(command) {
   if (command == "duplicate-tab") {
-    chrome.tabs.query({
-      currentWindow: true,
-      active: true,
-    }, function(tabs) {
-      var tab = tabs[0];
-      chrome.storage.sync.get(default_options, function(items) {
-        chrome.tabs.create({
-          active: !items.background,
-          index: tab.index+1,
-          openerTabId: tab.id,
-          url: tab.url
+    chrome.storage.sync.get(default_options, function(options) {
+      if (options.background) {
+        // Open tab in background
+        chrome.tabs.query({
+          currentWindow: true,
+          active: true,
+        }, function(tabs) {
+          var tab = tabs[0];
+          chrome.tabs.create({
+            active: false,
+            index: tab.index+1,
+            openerTabId: tab.id,
+            url: tab.url
+          });
         });
-      });
+      }
+      else {
+        // Open tab in foreground
+        chrome.tabs.query({
+          currentWindow: true,
+          active: true,
+        }, function(tabs) {
+          var tab = tabs[0];
+          chrome.tabs.duplicate(tab.id);
+        });
+      }
     });
   }
 });
