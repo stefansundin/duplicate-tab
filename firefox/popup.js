0094a1d3d8f36701ef8582a866517889309b3a32
Stefan Sundin <git@stefansundin.com>
2018-06-08 11:21:54 -0700

    Make the shortcut configurable in Firefox. It is a shame that they didn't copy the way that Chrome let's you customize keyboard shortcut.

--- firefox/popup.js
+++ firefox/popup.js
@@ -3,13 +3,26 @@ var default_options = {
 };
 
 document.addEventListener("DOMContentLoaded", function() {
+  var shortcut = document.getElementById("shortcut");
   chrome.commands.getAll(function(commands) {
     commands.forEach(function(command) {
       if (command.name == "duplicate-tab") {
-        var shortcut = document.getElementById("shortcut");
-        shortcut.textContent = command.shortcut || "not set";
+        shortcut.value = command.shortcut;
       }
     });
+  });
+
+  var save = document.getElementById("shortcut");
+  save.addEventListener("change", async function() {
+    document.getElementById("error").innerText = "";
+    try {
+      await browser.commands.update({
+        name: "duplicate-tab",
+        shortcut: shortcut.value
+      });
+    } catch (e) {
+      document.getElementById("error").innerText = e.toString();
+    }
   });
 
   var background = document.getElementById("background");
