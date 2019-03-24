901f5dc34b7983a28de11da5562e3ef496c734b9
Stefan Sundin <git@stefansundin.com>
2019-03-24 15:20:35 -0700

    Move all highlighted tabs. Preserve pinned state.

--- chrome/background.js
+++ chrome/background.js
@@ -84,43 +84,55 @@ chrome.commands.onCommand.addListener(function(command) {
     chrome.permissions.request({
       permissions: ["tabs"]
     }, function(granted) {
-      if (granted) {
-        const _currentTab = new Promise((resolve) => {
-          chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-            resolve(tabs[0]);
+      if (!granted) {
+        return;
+      }
+
+      (async () => {
+        const currentWindow = await new Promise((resolve) => {
+          chrome.windows.getCurrent((wnd) => {
+            resolve(wnd);
+          });
+        });
+        const tabs = await new Promise((resolve) => {
+          chrome.tabs.query({
+            currentWindow: true,
+            highlighted: true,
+          }, (tabs) => {
+            resolve(tabs);
           })
         });
 
-        const _currentWindow = new Promise((resolve) => {
-          chrome.windows.getCurrent((win) => resolve(win));
+        // Cherrypick the options we want to clone
+        const windowOpts = {
+          type: currentWindow.type,
+          incognito: currentWindow.incognito,
+          tabId: tabs[0].id, // this removes the tab from the old window
+          focused: true,
+        }
+
+        // Can't set dimensions and position for some states
+        if (["minimized", "maximized", "fullscreen"].includes(currentWindow.state)) {
+          windowOpts.state = currentWindow.state;
+        }
+        else {
+          windowOpts.top = currentWindow.top;
+          windowOpts.left = currentWindow.left;
+          windowOpts.height = currentWindow.height;
+          windowOpts.width = currentWindow.width;
+        }
+
+        chrome.windows.create(windowOpts, function(newWindow) {
+          chrome.tabs.move(tabs.slice(1).map((t) => t.id), {
+            windowId: newWindow.id,
+            index: -1,
+          });
+          tabs.filter((t) => t.pinned).forEach((t) => {
+            chrome.tabs.update(t.id, { pinned: true });
+          });
+          chrome.tabs.update(tabs.find((t) => t.active).id, { active: true });
         });
-
-        (async () => {
-          const currentTab = await _currentTab
-          const currentWindow = await _currentWindow
-
-          // Let's cherrypick the options we want to clone
-          const windowOpts = {
-            tabId: currentTab.id,
-            focused: true,
-            incognito: currentWindow.incognito,
-            type: currentWindow.type
-          }
-
-          // Cant set dimensions and pos manually for some states
-          if (['minimized', 'maximized', 'fullscreen'].includes(currentWindow.state)) {
-            windowOpts.state = currentWindow.state;
-          }
-          else {
-            windowOpts.top = currentWindow.top;
-            windowOpts.left = currentWindow.left;
-            windowOpts.height = currentWindow.height;
-            windowOpts.width = currentWindow.width;
-          }
-
-          chrome.windows.create(windowOpts);
-        })();
-      }
+      })();
     });
   }
 });
