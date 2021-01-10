636d5a2fe8f691789b24c1a98132f1c6e4747a25
Stefan Sundin <git@stefansundin.com>
2021-01-09 22:05:42 -0800

    Remove custom shortcut handling in favor of the built-in Firefox shortcut manager. Unfortunately, it is not possible to link to it like in Chrome.

--- firefox/popup.js
+++ firefox/popup.js
@@ -2,67 +2,12 @@ const default_options = {
   background: false,
 };
 
-let all_commands = [];
-chrome.commands.getAll(function(commands) {
-  all_commands = commands.map((c) => c.name);
-});
-
-function refresh_shortcuts() {
-  chrome.commands.getAll(function(commands) {
-    commands.forEach(function(command) {
-      const shortcut = document.getElementById(`${command.name}-shortcut`);
-      if (!shortcut) return;
-      shortcut.value = command.shortcut || "";
-    });
-  });
-}
-
 document.addEventListener("DOMContentLoaded", function() {
-  refresh_shortcuts();
-
-  const save = document.getElementById("save");
-  save.addEventListener("click", function() {
-    if (document.getElementById("duplicate-to-new-window-shortcut").value != "") {
-      chrome.permissions.request({
-        permissions: ["tabs"]
-      }, function(granted) {
-        revoke.disabled = !granted;
-      });
-    }
-  });
-  save.addEventListener("click", async function() {
-    document.getElementById("error").innerText = "";
-    try {
-      for (let i=0; i < all_commands.length; i++) {
-        const command = all_commands[i];
-        const shortcut = document.getElementById(`${command}-shortcut`);
-        if (!shortcut) continue;
-        if (shortcut.value == "") {
-          await browser.commands.reset(command);
-        }
-        else {
-          await browser.commands.update({
-            name: command,
-            shortcut: shortcut.value,
-          });
-        }
-      }
-      refresh_shortcuts();
-    } catch (e) {
-      document.getElementById("error").innerText = e.toString();
-    }
-  });
-
-  // bind enter key to save button
   chrome.commands.getAll(function(commands) {
     commands.forEach(function(command) {
       const shortcut = document.getElementById(`${command.name}-shortcut`);
       if (!shortcut) return;
-      shortcut.addEventListener("keyup", function(e) {
-        if (e.keyCode == 13) {
-          save.click();
-        }
-      });
+      shortcut.textContent = command.shortcut || "not set";
     });
   });
 
